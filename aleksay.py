c726a15d63ebde7a79b69ecdf153af7827514e44
Anderson Felix <anderson.felix@grupofolha.com.br>
2015-11-27 20:18:12 -0200

    Remove elementos vazios da array quotes

--- aleksay.py
+++ aleksay.py
@@ -5,16 +5,17 @@ import  random, sys
 
 header_line="|||||||||||||| ALEKSAY ||||||||||||||"
 quotes=open("messages.txt").read().split("\n")
+quotes=filter(bool, quotes)
 
 # Alek
 def alek():
-	ballon="___________  __________"
-	ballon_footer="\/"
-	ballon='{s:{c}^{n}}'.format(s=ballon,n=len(header_line),c=' ')
-	print(ballon)
-	ballon_footer='{s:{c}^{n}}'.format(s=ballon_footer,n=len(header_line),c=' ')
-	print(ballon_footer)
-	print ("""
+    ballon="___________  __________"
+    ballon_footer="\/"
+    ballon='{s:{c}^{n}}'.format(s=ballon,n=len(header_line),c=' ')
+    print(ballon)
+    ballon_footer='{s:{c}^{n}}'.format(s=ballon_footer,n=len(header_line),c=' ')
+    print(ballon_footer)
+    print ("""
        |\\\\\\\\\\\\\\  !
        ||      )
        || ~~  ~~
@@ -25,29 +26,29 @@ def alek():
        |__  |         | ]]]
    __ /__/\/_\__      | / )
   /             \\     /  /
-	""")
+    """)
 
 # frase por parâmetro
 def quote():
-	q=sys.argv[1:]
-	if len(q) > 0:
-		print(" ".join(q).center( len(header_line) , " " ))
-	else:
-		print_random_quote()
+    q=sys.argv[1:]
+    if len(q) > 0:
+        print(" ".join(q).center( len(header_line) , " " ))
+    else:
+        print_random_quote()
 
 # frase randomica
 def print_random_quote():
-	quote=random.choice(quotes)
-	quote='{s:{c}^{n}}'.format(s=quote,n=len(header_line),c=' ')
-	print(quote)
+    quote=random.choice(quotes)
+    quote='{s:{c}^{n}}'.format(s=quote,n=len(header_line),c=' ')
+    print(quote)
 
 # main
 def main():
-	print("")
-	quote()
-	alek()
-	print(header_line)
-	print("")
+    print("")
+    quote()
+    alek()
+    print(header_line)
+    print("")
 
 # main
 if __name__ == "__main__":
